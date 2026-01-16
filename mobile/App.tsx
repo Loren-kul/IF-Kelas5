41555d569d70b8cd5d596179eb3573a9f27c41f8
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-16 23:47:18 +0700

    update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,5 @@
-import { View, Text } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>APLIKASI INFORMATIKA KELAS 5</Text>
-    </View>
-  );
+  return <AppNavigator />;
 }
