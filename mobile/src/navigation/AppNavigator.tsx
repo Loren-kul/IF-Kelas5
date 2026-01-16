1e353694c6349e5edc487a255c88906d7f57be07
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-16 23:56:17 +0700

    update AppNavigator.tsx

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -1,5 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { enableScreens } from "react-native-screens";
+
+enableScreens(); // ⬅️ WAJIB
 
 import LoginScreen from "../screens/LoginScreen";
 import SemesterScreen from "../screens/SemesterScreen";
