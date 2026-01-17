9ee1cbded14bece49562023f5449b912d85526fa
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 12:42:21 +0700

    update button.tsx

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -2,8 +2,16 @@ import { TouchableOpacity, Text } from "react-native";
 
 export default function Button({ title, onPress }: any) {
   return (
-    <TouchableOpacity onPress={onPress} style={{ padding: 10 }}>
-      <Text>{title}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: "#2196f3",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+      }}
+    >
+      <Text style={{ color: "white" }}>{title}</Text>
     </TouchableOpacity>
   );
 }
