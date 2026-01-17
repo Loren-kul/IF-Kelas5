9a4efd2010a662cecb680b88d72d7daff615c780
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 16:21:17 +0700

    update route.ts

--- api/src/app/api/auth/login/route.ts
+++ api/src/app/api/auth/login/route.ts
@@ -1,48 +1,33 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
 import prisma from "@/lib/prisma";
-import { signJwt } from "@/lib/jwt";
+import bcrypt from "bcryptjs";
+import { signToken } from "@/lib/jwt";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  if (!email || !password) {
-    return NextResponse.json(
-      { message: "Email dan password wajib diisi" },
-      { status: 400 }
-    );
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
+  const user = await prisma.user.findUnique({ where: { email } });
 
   if (!user) {
-    return NextResponse.json(
-      { message: "User tidak ditemukan" },
-      { status: 401 }
-    );
+    return NextResponse.json({ message: "Email tidak ditemukan" }, { status: 401 });
   }
 
   const valid = await bcrypt.compare(password, user.password);
 
   if (!valid) {
-    return NextResponse.json(
-      { message: "Password salah" },
-      { status: 401 }
-    );
+    return NextResponse.json({ message: "Password salah" }, { status: 401 });
   }
 
-  const token = signJwt({
+  const token = signToken({
     id: user.id,
-    email: user.email,
+    role: user.role,
   });
 
   return NextResponse.json({
     token,
     user: {
       id: user.id,
-      email: user.email,
+      name: user.name,
       role: user.role,
     },
   });
