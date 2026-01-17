dc319b59d6f1c220b96393fe4bf4582e8f344f65
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 19:02:32 +0700

    update api.ts di mobile

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.100.74:3000/api", // ganti IP jika pakai HP
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://192.168.100.74/api", // ganti IP backend
+});
+
+api.interceptors.request.use(async config => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
