7da5cb33f480e4152637e721c009da30af18b936
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 19:04:49 +0700

    update loginsreen setelah pindah posisi

--- mobile/src/auth/LoginScreen.tsx
+++ mobile/src/auth/LoginScreen.tsx
@@ -1,62 +1,39 @@
 import { View, Text, TextInput, Button, Alert } from "react-native";
 import { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import api from "../services/api";
 
 export default function LoginScreen({ navigation }: any) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
 
   const login = async () => {
-    if (!email || !password) {
-      Alert.alert("Validasi", "Email dan password wajib diisi");
-      return;
-    }
-
-    setLoading(true);
-
     try {
-      const res = await api.post("/auth/login", {
-        email,
-        password,
-      });
+      const res = await api.post("/auth/login", { email, password });
 
-      console.log("LOGIN RESPONSE:", res.data);
+      await AsyncStorage.setItem("token", res.data.token);
+      await AsyncStorage.setItem("role", res.data.user.role);
 
-      if (res.data.token) {
-        Alert.alert("Login berhasil");
-        navigation.replace("Semester", {
-          token: res.data.token,
-        });
+      if (res.data.user.role === "GURU") {
+        navigation.replace("GuruHome");
       } else {
-        Alert.alert("Login gagal", "Token tidak ditemukan");
+        navigation.replace("Semester");
       }
-    } catch (err: any) {
-      console.log("LOGIN ERROR:", err.response?.data || err.message);
+    } catch {
       Alert.alert("Login gagal", "Email atau password salah");
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
     <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 20, marginBottom: 20 }}>
-        Login Siswa
-      </Text>
+      <Text>Login</Text>
 
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
         autoCapitalize="none"
-        keyboardType="email-address"
-        style={{
-          borderWidth: 1,
-          marginBottom: 10,
-          padding: 10,
-          borderRadius: 5,
-        }}
+        style={{ borderWidth: 1, marginBottom: 10 }}
       />
 
       <TextInput
@@ -64,19 +41,10 @@ export default function LoginScreen({ navigation }: any) {
         value={password}
         onChangeText={setPassword}
         secureTextEntry
-        style={{
-          borderWidth: 1,
-          marginBottom: 20,
-          padding: 10,
-          borderRadius: 5,
-        }}
+        style={{ borderWidth: 1, marginBottom: 10 }}
       />
 
-      <Button
-        title={loading ? "Loading..." : "Login"}
-        onPress={login}
-        disabled={loading}
-      />
+      <Button title="Login" onPress={login} />
     </View>
   );
 }
