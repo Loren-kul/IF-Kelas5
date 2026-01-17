bc384110ceda305e83fdffb4a9ec7ec7dd448e2c
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 16:41:52 +0700

    update route.ts pada auth

--- api/src/app/api/auth/login/route.ts
+++ api/src/app/api/auth/login/route.ts
@@ -7,16 +7,12 @@ export async function POST(req: Request) {
   const { email, password } = await req.json();
 
   const user = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) {
-    return NextResponse.json({ message: "Email tidak ditemukan" }, { status: 401 });
-  }
+  if (!user)
+    return NextResponse.json({ message: "Email salah" }, { status: 401 });
 
   const valid = await bcrypt.compare(password, user.password);
-
-  if (!valid) {
+  if (!valid)
     return NextResponse.json({ message: "Password salah" }, { status: 401 });
-  }
 
   const token = signToken({
     id: user.id,
