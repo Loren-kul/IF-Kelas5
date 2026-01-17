2e6946f7e1fae0d4015a3720e77f36ad9151691a
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-01-17 16:11:36 +0700

    update seed.ts untuk role

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,35 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
+  const password = await bcrypt.hash("123456", 10);
 
-  const passwordGuru = await bcrypt.hash("guru123", 10);
-  const passwordSiswa = await bcrypt.hash("siswa123", 10);
-
-  await prisma.user.create({
-    data: {
-      nama: "Guru Informatika",
-      email: "guru@ifkelas5.com",
-      password: passwordGuru,
-      role: "GURU",
-    },
+  await prisma.user.createMany({
+    data: [
+      {
+        name: "Guru Informatika",
+        email: "guru@gmail.com",
+        password,
+        role: "GURU",
+      },
+      {
+        name: "Siswa Kelas 5",
+        email: "siswa@gmail.com",
+        password,
+        role: "SISWA",
+      },
+    ],
   });
-
-  await prisma.user.create({
-    data: {
-      nama: "Siswa Kelas 5",
-      email: "siswa@ifkelas5.com",
-      password: passwordSiswa,
-      role: "SISWA",
-    },
-  });
-
-  console.log("✅ Seeder berhasil");
 }
 
-main()
-  .catch(console.error)
-  .finally(() => prisma.$disconnect());
+main();
